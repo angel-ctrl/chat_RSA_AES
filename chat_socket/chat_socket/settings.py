265c7dbd711ad82c9fab4819eb769e80d12d21dc
angel-ctrl <angelalfredomanrique@hotmail.com>
2022-07-18 05:15:23 -0400

    si

--- chat_socket/chat_socket/settings.py
+++ chat_socket/chat_socket/settings.py
@@ -121,7 +121,7 @@ STATIC_URL = '/static/'
 #STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 STATICFILES_DIRS = [
-    "C:/Users/angel/PycharmProjects/chat/static"
+    "C:/Users/angel/PycharmProjects/chat_RSA_AES/static"
 ]
 
 # Default primary key field type
